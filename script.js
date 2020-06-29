9018a7bf95f2c43f0edb7914ac3f9ae1e8b30d9d
thomasmartuaps <thomasmartuaps@gmail.com>
2020-06-30 04:56:23 +0700

    Sorting done.

--- script.js
+++ script.js
@@ -6,16 +6,28 @@ let emptyDiv = new Promise((resolve, reject) => {
   resolve()
 })
 
-// Neccessary because GitHub's date format is different from Javascript's, apparently
-function dateConverter(gitHubDate) {
-  let getDate = gitHubDate.split('T')
-  let spacedDate = getDate[0].split('-')
-  let month = spacedDate[1][1]
-  spacedDate[1] = library[month]
-  let date = spacedDate.reverse().join(' ')
-
-  return date
+// This function sort repos by date.
+function sortRepos(repositories) {
+  repositories.forEach(el => {
+    let date = new Date(el.updated_at)
+    el.milisecs = date.getTime()
+  })
+  return repositories.sort((a, b) => b.milisecs - a.milisecs)
 }
+
+// Neccessary because GitHub's date format is different from Javascript's, apparently
+
+// function dateConverter(gitHubDate) {
+//   let getDate = gitHubDate.split('T')
+//   let spacedDate = getDate[0].split('-')
+//   let month = spacedDate[1][1]
+//   spacedDate[1] = library[month]
+//   let date = spacedDate.reverse().join(' ')
+
+//   return date
+// }
+
+// Function above not currently needed but I'll keep them here for when I decide to use them again.
 
 $(document).ready(function() {
   emptyDiv.then(
@@ -28,9 +40,12 @@ $(document).ready(function() {
       }
     })
     .done(response => {
-      for(i=0; i < response.length; i++) {
-        console.log(response[i])
-        let date = dateConverter(response[i].updated_at)
+      const sortedRepos = sortRepos(response)
+      for(i=0; i < sortedRepos.length; i++) {
+        // Converts date
+        const d = new Date(sortedRepos[i].updated_at)
+        const date = d.toDateString()
+        console.log('date string', date)
         // Decides whether this repo gets shown in the left or right side of timeline
         let position = ''
         if(i % 2 === 0) position = 'left'
@@ -39,9 +54,13 @@ $(document).ready(function() {
         $('.timeline').append(`
           <div class="container ${position}">
             <div class="content">
-              <h2>${response[i].name}</h2>
-              <h3>${date}</h3>
-              <p>Click <a href="${response[i].url}">here</a> to visit repo</p>
+              <h2>
+                ${date}
+              </h2>
+              <h3>
+                ${sortedRepos[i].name}
+              </h3>
+              <p>Click <a href="${sortedRepos[i].url}">here</a> to visit repo</p>
 
             </div>
           </div>
